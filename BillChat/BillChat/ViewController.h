c997be9dba0c0b9da5ba90fb1efe2ca0ebce81f1
Ariel Melendez <ariel.l.melendez@gmail.com>
2016-05-13 18:24:46 -0400

    Snapshot initial state of the BillChat project
    Working off a clean slate makes life easier for everyone. The project intends
    to use Core Data for persistence, though a wrapper has not yet been chosen.

---
+++ BillChat/BillChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillChat
+//
+//  Created by Ariel Melendez on 5/13/16.
+//  Copyright © 2016 ALM Projects. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
