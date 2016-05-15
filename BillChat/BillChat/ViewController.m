3b7d1c130514c48348e5d6795d3af6ec3320b68a
Ariel Melendez <ariel.l.melendez@gmail.com>
2016-05-14 21:01:22 -0400

    Basic setup for ViewController
    Added an empty UICollectionView and a view container to house and serve as a
    backdrop for the chat UITextField. Hooked up some basic constraint manipulation
    to move the container around as the keyboard shows and hides and setup a
    callback to handle when the user hits the Send key.

--- BillChat/BillChat/ViewController.m
+++ BillChat/BillChat/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UICollectionViewDelegate, UICollectionViewDataSource, UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UICollectionView *chatCollectionView;
+@property (weak, nonatomic) IBOutlet UITextField *chatTextField;
+@property (weak, nonatomic) IBOutlet UIView *chatEntryContainer;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *keyboardHeightConstraint;
 
 @end
 
@@ -16,12 +21,116 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupCollectionView];
+    [self setupTextField];
+    [self startObservingKeyboard];
+    [self setupKeyboardAutoDismisser];
 }
+
+- (void) dealloc {
+    [self stopObservingKeyboard];
+}
+
+- (void)setupCollectionView {
+    self.chatCollectionView.dataSource = self;
+    self.chatCollectionView.delegate = self;
+}
+
+- (void)setupTextField {
+    self.chatTextField.delegate = self;
+}
+
+- (void)startObservingKeyboard {
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)stopObservingKeyboard {
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)setupKeyboardAutoDismisser {
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+}
+
+- (void)dismissKeyboard {
+    [self.chatTextField resignFirstResponder];
+}
+
+- (void)keyboardWillShow:(NSNotification*)notification {
+    NSDictionary *userInfo = notification.userInfo;
+    NSValue *keyboardFrameValue = [userInfo objectForKey:UIKeyboardFrameEndUserInfoKey];
+    NSTimeInterval animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    CGRect keyboardFrameRect = [keyboardFrameValue CGRectValue];
+    CGFloat keyboardHeight = keyboardFrameRect.size.height;
+
+    // Because the "space" is actually the difference between the bottom lines of the 2 views,
+    // we need to set a negative constant value here.
+    self.keyboardHeightConstraint.constant = -keyboardHeight;
+    
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+
+- (void)keyboardWillHide:(NSNotification*)notification {
+    NSDictionary *userInfo = notification.userInfo;
+    NSTimeInterval animationDuration = [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
+    
+    self.keyboardHeightConstraint.constant = 0;
+    [UIView animateWithDuration:animationDuration animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+// Dismiss the keyboard if the user touches anything but the views in the text entry container area
+/*- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [[event allTouches] anyObject];
+    if ([self.chatTextField isFirstResponder] && ![[touch view] isDescendantOfView:self.chatEntryContainer]) {
+        [self.chatTextField resignFirstResponder];
+    }
+
+    [super touchesBegan:touches withEvent:event];
+}*/
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - UICollectionViewDelegate
+
+// TODO: Decide if we need any callbacks
+
+#pragma mark - UICollectionViewDataSource
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    // TODO: Implement!
+    return 0;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    // TODO: Implement!
+    return nil;
+}
+
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    NSLog(@"SEND THE MESSAGE! Message = %@", textField.text);
+    textField.text = @"";
+    
+    return YES;
+}
+
 
 @end
